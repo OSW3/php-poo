e5700fc343b61a276ae74f7d39d9e3836317e0b6
Arnaud BODEL <contact@osw3.net>
2020-03-31 14:59:12 +0200

    -

---
+++ TP-Batman-vs-Superman/Personnage.php
@@ -0,0 +1,25 @@
+<?php 
+
+class Personnage
+{
+    // Defiition des constantes
+    // --
+
+    /**
+     * Niveau novice
+     * @param int
+     */
+    const NOVICE = 1; 
+
+    /**
+     * Niveau Medium
+     * @param int
+     */   
+    const MEDIUM = 2;  
+
+    /**
+     * Niveau Expert
+     * @param int
+     */  
+    const EXPERT = 3;    
+}
