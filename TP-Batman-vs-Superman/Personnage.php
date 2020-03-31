4d6af39ced6fa7026442f133ad5f0650dac83d31
Arnaud BODEL <contact@osw3.net>
2020-03-31 17:36:37 +0200

    -

--- TP-Batman-vs-Superman/Personnage.php
+++ TP-Batman-vs-Superman/Personnage.php
@@ -52,33 +52,148 @@ class Personnage
     // Methods
     // --
 
-    public function __construct()
+    public function __construct(string $name, int $xp)
     {
+        // Definir les propriétés
+        // $this->setName($name);
+        // $this->setXp($xp);
+
+        // Définir les propriétés en méthodes chainées
+        $this
+            ->setName($name)
+            ->setXp($xp)
+        ;
     }
 
-    public function sayHello()
+    /**
+     * Say hello to the opponent
+     *
+     * @param Personnage $opponent
+     * @return string
+     */
+    public function sayHello(Personnage $opponent): string
     {
+        return $this->name . " salue " . $opponent->name;
     }
 
-    public function attack()
+    /**
+     * Attack an opponent
+     *
+     * @param Personnage $opponent
+     * @param integer $coef
+     * @return self
+     */
+    public function attack(Personnage $opponent, int $coef=1): self
     {
+        switch ($this->xp)
+        {
+            case self::NOVICE: // 1
+                $opponent->setHp( $opponent->getHp() - (10*$coef) );
+                break;
+
+            case self::MEDIUM: // 2
+                $opponent->setHp( $opponent->getHp() - (20*$coef) );
+                break;
+
+            case self::EXPERT: // 3
+                $opponent->setHp( $opponent->getHp() - (30*$coef) );
+                break;
+        }
+
+        $this->levelUp($opponent);
+
+        return $this;
     }
 
-    public function superAttack()
+    /**
+     * Super attack = Attack x 2
+     *
+     * @param Personnage $opponent
+     * @return self
+     */
+    public function superAttack(Personnage $opponent): self
     {
+        $this->attack($opponent, 2);
+
+        return $this;
     }
 
-    public function secretAttack()
+    /**
+     * Set oppenent HP to 0 if oppenent HP less than 50
+     *
+     * @param Personnage $opponent
+     * @return self
+     */
+    public function secretAttack(Personnage $opponent): self
     {
+        if ($opponent->hp < 50)
+        {
+            $opponent->setHp(0);
+        }
+
+        $this->levelUp($opponent);
+
+        return $this;
     }
 
-    public function care()
+    /**
+     * Add 10 Health point to the character
+     *
+     * @return self
+     */
+    public function care(): self
     {
+        $this->hp += 10;
+
+        return $this;
     }
 
-    public function levelUp()
+    private function levelUp(Personnage $opponent): self
     {
+        if ($opponent->getHp() === 0)
+        {
+            switch ($this->getXp())
+            {
+                case self::NOVICE:
+                    $this->setXp( self::MEDIUM );
+                break;
+
+                case self::MEDIUM:
+                    $this->setXp( self::EXPERT );
+                break;
+
+                case self::EXPERT:
+                    // Do nothing
+                    // Already the Boss
+                break;
+
+                default:
+                    $this->setXp( self::NOVICE );
+            }
+        }
+
+        return $this;
     }
+
+
+    /**
+     * Health point never less than 0
+     *
+     * @param Personnage $entity
+     * @return self
+     */
+    private function unsignedHp(Personnage $entity): self
+    {
+        if ($entity->hp < 0)
+        {
+            $entity->hp = 0;
+        }
+
+        return $this;
+    }
+
+
+
 
 
     /**
@@ -98,7 +213,7 @@ class Personnage
      *
      * @return  self
      */ 
-    public function setName(string $name)
+    private function setName(string $name)
     {
         $this->name = $name;
 
@@ -110,7 +225,7 @@ class Personnage
      *
      * @return  integer
      */ 
-    public function getHp()
+    public function getHp(): int
     {
         return $this->hp;
     }
@@ -126,6 +241,8 @@ class Personnage
     {
         $this->hp = $hp;
 
+        $this->unsignedHp( $this );
+
         return $this;
     }
 
@@ -134,7 +251,7 @@ class Personnage
      *
      * @return  int
      */ 
-    public function getXp()
+    public function getXp(): int
     {
         return $this->xp;
     }
--- TP-Batman-vs-Superman/index.php
+++ TP-Batman-vs-Superman/index.php
@@ -1,11 +1,103 @@
+<?php
+include "Personnage.php";
+
+$batman = new Personnage("Batman", Personnage::MEDIUM);
+$superman = new Personnage("Superman", Personnage::NOVICE);
+
+function score ($opponent_1, $opponent_2) {
+    return "<small>(".$opponent_1->getName()." : ".$opponent_1->getHp()." / ".$opponent_2->getHp()." : ".$opponent_2->getName().")</small>";
+}
+?>
 <!DOCTYPE html>
-<html lang="en">
+<html lang="fr">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>xxx vs xxx</title>
+    <title><?= $batman->getName()?> vs. <?= $superman->getName()?></title>
 </head>
 <body>
     
+    <h1><?= $batman->getName()?> vs. <?= $superman->getName()?></h1>
+
+    <h2>Présentation</h2>
+
+    <fieldset>
+        <legend>Personnage 1</legend>
+        <div>Nom : <?= $batman->getName() ?></div>
+        <div>Points de vie : <?= $batman->getHp() ?></div>
+        <div>Experience : <?= $batman->getXp() ?></div>
+    </fieldset>
+
+    <fieldset>
+        <legend>Personnage 2</legend>
+        <div>Nom : <?= $superman->getName() ?></div>
+        <div>Points de vie : <?= $superman->getHp() ?></div>
+        <div>Experience : <?= $superman->getXp() ?></div>
+    </fieldset>
+
+
+    <hr>
+
+    <h2>Salutation</h2>
+
+    <div>
+        <?= $batman->sayHello($superman) ?>
+        <?= score($batman, $superman) ?>
+    </div>
+    <div>
+        <?= $superman->sayHello($batman) ?>
+        <?= score($batman, $superman) ?>
+    </div>
+
+
+    <hr>
+
+    <h2>Début du combat</h2>
+
+    <h3>Batman attaque Superman</h3>
+    <?php $batman->attack($superman) ?>
+    <?= score($batman, $superman) ?>
+
+    <h3>Superman riposte : Attaque + SuperAttaque</h3>
+    <?php $superman->attack($batman)->superAttack($batman) ?>
+    <?= score($batman, $superman) ?>
+
+    <h3>Une magnifique SuperAttaque de Batman !!</h3>
+    <?php $batman->superAttack($superman) ?>
+    <?= score($batman, $superman) ?>
+
+    <h3>Superman pleure sa maman... et se soigne.</h3>
+    <?php $superman->care() ?>
+    <?= score($batman, $superman) ?>
+
+    <h3>Batman tente une attaque secrete (mais... dans le vent).</h3>
+    <?php $batman->secretAttack($superman) ?>
+    <?= score($batman, $superman) ?>
+
+    <h3>Superman (la tête dans le sac) se défend avec un double attaque..... trop pitoyable :p.</h3>
+    <?php $superman->attack($batman)->attack($batman) ?>
+    <?= score($batman, $superman) ?>
+
+    <h3>Batman lance une simple attaque puis achève l'alien avec une attaque secrète.</h3>
+    <?php $batman->attack($superman)->secretAttack($superman) ?>
+    <?= score($batman, $superman) ?>
+
+
+    <h2>Resultat</h2>
+
+    <fieldset>
+        <legend>Personnage 1</legend>
+        <div>Nom : <?= $batman->getName() ?></div>
+        <div>Points de vie : <?= $batman->getHp() ?></div>
+        <div>Experience : <?= $batman->getXp() ?></div>
+    </fieldset>
+
+    <fieldset>
+        <legend>Personnage 2</legend>
+        <div>Nom : <?= $superman->getName() ?></div>
+        <div>Points de vie : <?= $superman->getHp() ?></div>
+        <div>Experience : <?= $superman->getXp() ?></div>
+    </fieldset>
+
 </body>
 </html>
